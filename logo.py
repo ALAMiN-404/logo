44bba8c0f9c672bcc32ea341bf42f66320ad7d30
Cyber ALAMiN <100506616+Cyber-ALAMiN@users.noreply.github.com>
2022-04-17 15:43:11 +0600

    Create logo.py

---
+++ logo.py
@@ -0,0 +1,36 @@
+import os
+os.system("clear")
+G="\033[1;32m"
+B="\033[1;34m"
+Y="\033[1;33m"
+P="\033[1;35m"
+C="\033[1;36m"
+
+design="""
+{G}╔═╗ ╔╗     ╔╗      {C}╔╗ ╔╗     ╔╗       {Y}╔═══╗╔╗
+{G}║║╚╗║║     ║║      {C}║║ ║║     ║║       {Y}║╔═╗╠╝║
+{G}║╔╗╚╝╠══╦══╣╚═╗    {C}║╚═╝╠══╦══╣║╔╦══╦╩╣{Y}  ╔╝╠╗║
+{G}║║╚╗║║╔╗║╔╗║╔╗╠{P} ══ {C}╣╔═╗║╔╗║╔═╣╚╝╣║═╣╔╝{Y}  ║╔╝║║
+{G}║║ ║║║╚╝║╚╝║╚╝╠{P} ══ {C}╣║ ║║╔╗║╚═╣╔╗╣║═╣║ {Y}  ║║╔╝╚╗
+{G}╚╝ ╚═╩══╩══╩══╝   {C} ╚╝ ╚╩╝╚╩══╩╝╚╩══╩╝ {Y}  ╚╝╚══╝
+""".format(P=P,C=C,G=G,Y=Y) 
+
+
+logo=("""{B}
+**************************************************
+{G}Remember >>  •••Allah Always Watching You•••
+{B}**************************************************
+{C} ____We Work To Sarve You a Clean Cyber Space____
+{Y}   ____Well Come To Noob-Hacker's World____
+{B}**************************************************
+{ascii}
+{B}**************************************************
+{G}Author   >>  Tahsan-Nayem [Noob-Hacker71]
+{Y}Facebook >>  https://facebook.com/Noob.Hacker71
+{C}Github   >>  https://github.com/Noob-Hacker71
+{B}**************************************************
+""".format(ascii=design,B=B,C=C,G=G,Y=Y))
+
+User="Cyber"
+Pwd="ALAMiN"
+Permission="On"
